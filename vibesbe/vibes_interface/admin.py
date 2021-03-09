5001e14867929d7fb37ed0a86f5529f246e0104c
brycemooore <brycemooore@gmail.com>
2021-03-08 22:30:15 -0500

    some views

--- vibesbe/vibes_interface/admin.py
+++ vibesbe/vibes_interface/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
+from .models import User, Song
+
+admin.site.register(User)
+admin.site.register(Song)
 # Register your models here.
--- vibesbe/vibes_interface/views.py
+++ vibesbe/vibes_interface/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.core.serializers import serialize
 # Create your views here.
+from django.http import HttpResponse
+from .models import User, Song
+
+
+def index(request):
+    # s = Song(title="Hello", artist="Hello")
+    # s.save()
+    # u = User(username="John", email="brycemooore@gmail.com")
+    # u.save()
+    # u = User.objects.first().song_set.all()
+    # # u.songs.add(Song.objects.get(title="Hello"))
+    # data = serialize('json', [u], fields=('username', 'email', 'songs'))
+    return HttpResponse(data, content_type='application/json')
+
